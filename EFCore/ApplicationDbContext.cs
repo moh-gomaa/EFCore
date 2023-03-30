b94bbf79a60688874920de7387eb6b584177a99a
Mohamed Gomaa <mohamed.adel36@msa.edu.eg>
2023-03-30 09:05:20 +0200

    -Add DbContext which links the application layer with database layer.

---
+++ EFCore/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer("Data Source=DESKTOP-4ETF2Q1;Integrated Security=True");
+    }
+}
